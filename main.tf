6400cb311fd9026ec5f34e211bd1bf3c61e429fe
Rakibul Islam <rakibulroyal@gmail.com>
2025-03-24 02:55:17 +0100

    userdata, variables, windowstemolate file added

--- main.tf
+++ main.tf
@@ -81,6 +81,7 @@ resource "aws_instance" "dev_node" {
     key_name = aws_key_pair.mtc_auth.id
     vpc_security_group_ids = [aws_security_group.mtc_sg.id]
     subnet_id = aws_subnet.mtc_public_subnet.id
+    user_data = file("userdata.tpl")
     
     root_block_device {
       volume_size = 10
@@ -88,6 +89,15 @@ resource "aws_instance" "dev_node" {
   
     tags = {
       Name = " dev-node"
+    }
+
+    provisioner "local-exec" {
+      command = templatefile("${var.host_os}-ssh-config.tpl", {
+        hostname = self.public_ip,
+        user = "ubuntu",
+        identityfile = "~/.ssh/mtckey"
+      })
+      interpreter = [ "powershell", "-Command" ]
     }
 
     
